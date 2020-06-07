1b629ee656e6a3cf72d5af27bdddaf622c28e0d5
skine134 <skine134@naver.com>
2020-06-07 12:57:45 +0900

    remove unnessesary files

--- study_algorithm/test dynamicplan.py
+++
@@ -1,8 +0,0 @@
-def fib(n):
-    m=map(0->1,1->1)
-    if n not in keys(m):
-        m[n]=fib(n-1)+fib(n-2)
-    return m[n]
-print(fib(4))
-
-
--- study_algorithm/손익 분기점.py
+++
@@ -1,8 +0,0 @@
-static_value,dynamic_value,sale_value=map(int, input().split())
-total_sale=sale_value-dynamic_value
-if(total_sale<=0):
-    print(-1)
-elif(static_value==total_sale):
-    print(1)
-else:
-    print(int(static_value/total_sale)+1)
